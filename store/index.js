0ad81b9885183cb73cd2adb4708a03eacad288c7
YOU SHIH WEI <yu.leo@zigexn.co.jp>
2020-08-30 20:10:47 +0900

    clear token if expire

--- store/index.js
+++ store/index.js
@@ -78,19 +78,13 @@ const createStore = () => {
         ).then(result => {
           vuexContext.commit('setToken', result.idToken);
           localStorage.setItem("token", result.idToken);
-          localStorage.setItem("tokenExpiration", new Date().getTime() + result.expiresIn * 1000);
+          localStorage.setItem("tokenExpiration", new Date().getTime() + Number.parseInt(result.expiresIn) * 1000);
           Cookie.set('jwt', result.idToken);
-          Cookie.set('expirationDate', new Date().getTime() + result.expiresIn * 1000);
-          vuexContext.dispatch('setLogoutTimer', result.expiresIn * 1000)
+          Cookie.set('expirationDate', new Date().getTime() + Number.parseInt(result.expiresIn) * 1000);
         })
         .catch(err => {
           console.log(err);
         });
-      },
-      setLogoutTimer(vuexContext, duration) {
-        setTimeout( () => {
-          vuexContext.commit("clearToken")
-        }, duration)
       },
       initAuth(vuexContext, req) {
         let token;
@@ -115,13 +109,13 @@ const createStore = () => {
         } else {
           token = localStorage.getItem("token");
           jwtExpirationDate = localStorage.getItem("tokenExpiration");
-
-          if ( new Date().getTime() > +jwtExpirationDate || !token) {
-            return;
-          }
         }
 
-        vuexContext.dispatch("setLogoutTimer", +jwtExpirationDate - new Date().getTime());
+        if ( new Date().getTime() > +jwtExpirationDate || !token) {
+          console.log('No token or invalid token');
+          vuexContext.commit('clearToken');
+          return;
+        }
         vuexContext.commit("setToken", token);
       }
     },
